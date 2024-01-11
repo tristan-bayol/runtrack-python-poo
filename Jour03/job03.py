fc24b3629b17dda8dc805e4a8cde3ffac04d422e
Tristan Bayol <tristan-bayol@laplateforme.io>
2024-01-11 13:38:55 +0100

    Tasklist start

--- Jour03/job03.py
+++ Jour03/job03.py
@@ -2,7 +2,21 @@ class Tache:
     def __init__ (self, titre, description):
         self.titre = titre
         self.desc = description
+        self.statut = False
+
+    def endtask (self):
         self.statut = True
 
+    def infostask(self):
+        return "Tâche à faire" if not self.statut else "Tâche terminée"
 
-class ListDeTache :
+    # retourne une chaîne de caractères représentant les informations sur la tâche initié dans le constructeur
+    def __str__(self):
+        return f"Tâche: {self.titre}\nDescription: {self.desc}\nStatut: {self.infostask()}"
+
+# class ListDeTache :
+            
+manger = Tache("manger", "je dois manger")
+print (manger)
+manger.endtask()
+print (manger)
